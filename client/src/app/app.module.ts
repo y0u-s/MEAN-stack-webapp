6a2310fca1fb7d0080b9d4a0c0c350a80f78b29e
y0u-s <youssef.lakdime@gmail.com>
2018-06-18 12:42:22 +0200

    Add device to room. If room already has a device, show its device_id to the user

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {AppRoutingModule} from "./app-routing-module";
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {AuthService} from "./services/auth.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HttpClientModule
+
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
 const path = require('path')
+const authentication = require('./routes/authentication')(router)
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://userYoussef:3ef82917@ds141720.mlab.com:41720/oefentoetsendb', (err) => {
@@ -13,7 +23,19 @@ mongoose.connect('mongodb://userYoussef:3ef82917@ds141720.mlab.com:41720/oefento
     }
 });
 
+
+app.use(cors({
+        origin: 'http://localhost:4200'
+    })
+);
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/client/dist/client'))
+app.use('/authentication', authentication)
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
